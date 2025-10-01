87ac6ebb8d079287329fd4024da4359c92bfa4db
EduardoDLCJ <ojjkd27@gmail.com>
2025-10-01 15:04:14 -0600

    ajuste despliegue

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch('http://192.168.100.15:4001/users/login', {
+      const response = await fetch('https://pwa-backend-knbm.onrender.com/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
